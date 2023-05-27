419b2e606822559754167d94acbf6d51d11e8551
Abdulah Tilal <abdullahtilal26.com>
2023-05-27 16:54:07 +0500

    included typeorm module on root to be applied to all sub modules and passed the config object created into module of typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TaskModule } from './task/task.module';
+import { typeOrmConfig } from './config/typeorm.config';
+import { TypeOrmModule } from '@nestjs/typeorm/dist';
 
 @Module({
-  imports: [TaskModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig),TaskModule],
 
 })
 export class AppModule {}
