c6ab785c9d64d872270ee65c61ca4f2dd3311d5f
Abdulah Tilal <abdullahtilal26.com>
2023-05-27 17:17:24 +0500

    added TaskRepository in TaskModule so that its available through this module via dependecy injection.Now we are ready to consume this in to task services

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -1,8 +1,11 @@
+import { TypeOrmModule } from '@nestjs/typeorm/dist';
 import { Module } from '@nestjs/common';
 import { TaskController } from './task.controller';
 import { TaskService } from './task.service';
+import { TaskRepository } from './task.repository';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([TaskRepository])],
   controllers: [TaskController],
   providers: [TaskService]
 })
